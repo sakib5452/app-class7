e4ce788da588cad57337ec0d40ffe2950e4ba6aa
najmul sakib <najmus191@gmail.com>
2023-11-23 12:39:21 +0600

    Whole loop

--- src/main/kotlin/condition.kt
+++ src/main/kotlin/condition.kt
@@ -1,10 +1,21 @@
 
 fun main(){
 
-var num = 100
-    while (num <= 110){
-        println(num)
-        num++
+//var num = 100
+//    while (num <= 110){
+//        println(num)
+//        num++
+//    }
+
+    var names = arrayOf("sakib","Kazi","jamiul","Dipu","Imran","gazi")
+    var number = 0
+
+    while(number < names.size){
+        println(names[number])
+        number++
     }
+
+
+
 
 }
